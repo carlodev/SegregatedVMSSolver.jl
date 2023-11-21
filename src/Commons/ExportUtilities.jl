b690c1ad303bfb300c3979d7eaf5790a07c6a5e1
carlodev <carlobrunellibs@gmail.com>
2023-11-21 16:55:02 +0100

    rewrite export, now single file

--- src/Commons/ExportUtilities.jl
+++ src/Commons/ExportUtilities.jl
@@ -24,7 +24,7 @@ end
 """
     conv_to_df(vv::Vector)
 
-Convert a Vector{Vector} to a DataFrame. It is used for export velocity field.
+Convert a Vector{Vector} to a DataFrame. It is used for export nodes, normals.
 """
 function conv_to_df(vv::Vector)
     d = get_dimension(vv)
@@ -69,9 +69,36 @@ function export_time_step(t::Float64, vv::Vector, fname::String, part::Int64)
     CSV.write(filename, df)
 end
 
+"""
+    export_time_step(t::Float64, vv::Vector, fname::String)
+
+Save .csv file global nodes
+"""
+function export_time_step(t::Float64, vv::Vector, fname::String)
+    df = conv_to_df(vv)
+    mkpath("Results")
+    filename = joinpath("Results", "$(fname)_$t.csv")
+    CSV.write(filename, df)
+end
 
 
-function export_nodes(parts, trian)
+function extract_global_unique(dfield, parts, global_unique_idx, timestep::Float64, fieldname::String)
+    gfield = gather(dfield)
+    map(gfield,parts) do g, part
+        if part ==1 #On Main procs
+            values_uniques = g.data[global_unique_idx]
+            export_time_step(timestep, values_uniques, fieldname)          
+        end
+    end
+end
+
+
+"""
+    get_local_unique_idx(parts, trian)
+
+For each part (aka processors), only non-duplicated nodes indexes are extracted.
+"""
+function get_local_unique_idx(parts, trian)
     f = (reffe) -> Gridap.Geometry.UnstructuredGrid(reffe)
 
     #export nodes
@@ -79,10 +106,10 @@ function export_nodes(parts, trian)
         ref_grids = map(f, Gridap.Geometry.get_reffes(ttrian))
         visgrid = Gridap.Visualization.VisualizationGrid(ttrian, ref_grids)
         visgrid_ = conv_VectorValue.(visgrid.sub_grid.node_coordinates)
-        nodes_tri = unique(visgrid_)
-
-        unique_idx = unique(i -> visgrid_[i], eachindex(visgrid_))
-        export_time_step(0.0, nodes_tri, "nodes", part)
+        nodes_tri = unique(visgrid_) #Coordinate of unique nodes
+ 
+        unique_idx = unique(i -> visgrid_[i], eachindex(visgrid_)) #Indexes of unique nodes on each part
+        # export_time_step(0.0, nodes_tri, "nodes", part)
 
         return unique_idx
     end
@@ -90,7 +117,51 @@ function export_nodes(parts, trian)
     return local_unique_idx
 end
 
-function export_n_Γ(params::Dict{Symbol,Any}, local_unique_idx)
+
+
+
+"""
+    export_nodes_glob(parts, trian)
+
+It gathers on MAIN procs (==1) the non-duplicates nodes for each procs. It computes the non-duplicate global indexes.
+It also extracts non-duplicated nodes.
+"""
+function export_nodes_glob(parts, trian)
+    f = (reffe) -> Gridap.Geometry.UnstructuredGrid(reffe)
+
+    #export nodes
+    local_unique_nodes = map(parts, trian.trians) do part, ttrian
+        ref_grids = map(f, Gridap.Geometry.get_reffes(ttrian))
+        visgrid = Gridap.Visualization.VisualizationGrid(ttrian, ref_grids)
+        visgrid_ = conv_VectorValue.(visgrid.sub_grid.node_coordinates)
+        nodes_tri = unique(visgrid_)
+        return nodes_tri
+    end
+
+    glun = gather(local_unique_nodes)
+
+    global_unique_idx = 0.0
+    map(glun,parts) do g,part
+        if part ==1 #On Main procs
+
+            nodes_uniques = unique(g.data)
+            export_time_step(0.0, nodes_uniques, "nodes")
+
+            global_unique_idx = unique(i -> g.data[i], eachindex(g.data)) 
+        end
+    end
+    return global_unique_idx
+end
+
+
+
+
+"""
+    export_n_Γ(params::Dict{Symbol,Any}, local_unique_idx, global_unique_idx)
+
+Export tangent and normal vectors at the corresponding points.
+"""
+function export_n_Γ(params::Dict{Symbol,Any}, local_unique_idx, global_unique_idx)
     #get unique values in each processor
 
     @unpack Γ,n_Γ, parts,force_tags = params
@@ -104,21 +175,29 @@ function export_n_Γ(params::Dict{Symbol,Any}, local_unique_idx)
         cellfields = Dict( "n_Γ" => n_Γ, "t_Γ"=>t_Γ)
 
         fdat = GridapDistributed._prepare_fdata(Γ.trians, cellfields)
-
-        map(parts, Γ.trians, fdat, local_unique_idx) do part, ttrian, cf, unique_idx
+        for field in keys(cellfields)
+        fieldh = map(parts, Γ.trians, fdat, local_unique_idx) do part, ttrian, cf, unique_idx
             ref_grids = map(f, Gridap.Geometry.get_reffes(ttrian))
             visgrid = Gridap.Visualization.VisualizationGrid(ttrian, ref_grids)
             pdata = Gridap.Visualization._prepare_pdata(ttrian, cf, visgrid.cell_to_refpoints)
-            for field in keys(cellfields)
+            field_h = 0.0
+            
             field_h = pdata[field][unique_idx]
-            export_time_step(0.0, field_h, field, part)
+            # export_time_step(0.0, field_h, field, part)
+            return field_h
             end
-        end
-    end
+         extract_global_unique(fieldh, parts, global_unique_idx, 0.0, field)
+    end #for
+    
+    end #end if
 end
 
+"""
+    export_fields(params::Dict{Symbol,Any}, local_unique_idx, global_unique_idx, tt::Float64, uh0, ph0)
 
-function export_fields(params::Dict{Symbol,Any}, local_unique_idx, tt::Float64, uh0, ph0)
+Export pressure and friction (not multiplied by viscosity) - airfoil simulations oriented
+"""
+function export_fields(params::Dict{Symbol,Any}, local_unique_idx, global_unique_idx, tt::Float64, uh0, ph0)
     #get unique values in each processor
 
     @unpack Γ,n_Γ, parts,force_tags = params
@@ -135,19 +214,24 @@ function export_fields(params::Dict{Symbol,Any}, local_unique_idx, tt::Float64, 
 
         fdat = GridapDistributed._prepare_fdata(Γ.trians, cellfields)
 
-        map(parts, Γ.trians, fdat, local_unique_idx) do part, ttrian, cf, unique_idx
-            ref_grids = map(f, Gridap.Geometry.get_reffes(ttrian))
-            visgrid = Gridap.Visualization.VisualizationGrid(ttrian, ref_grids)
-            pdata = Gridap.Visualization._prepare_pdata(ttrian, cf, visgrid.cell_to_refpoints)
-            for field in keys(cellfields)
-            field_h = pdata[field][unique_idx]
-            export_time_step(tt, field_h, field, part)
-            end
-        end
+        for field in keys(cellfields)
+            fieldh = map(parts, Γ.trians, fdat, local_unique_idx) do part, ttrian, cf, unique_idx
+                ref_grids = map(f, Gridap.Geometry.get_reffes(ttrian))
+                visgrid = Gridap.Visualization.VisualizationGrid(ttrian, ref_grids)
+                pdata = Gridap.Visualization._prepare_pdata(ttrian, cf, visgrid.cell_to_refpoints)
+                field_h = pdata[field][unique_idx]
+            return field_h 
+            end #end map
+            extract_global_unique(fieldh, parts, global_unique_idx, tt, field)
+        end #end for
     end
 end
 
+"""
+    rotation(n::VectorValue{2,Float64})
 
+It rotates by π/2 the n vector
+"""
 function rotation(n::VectorValue{2,Float64})
     n1,n2 = [n...]
     VectorValue(-n2,n1)
@@ -183,8 +267,30 @@ function forces_domain!(params)
 
     Γ = BoundaryTriangulation(model; tags=force_tags) 
     dΓ = Measure(Γ,degree)
-    n_Γ = get_normal_vector(Γ) #Check if the direction is inside or outdside the domain; probably a -1 coefficient needed
+    n_Γ = get_normal_vector(Γ)
     force_params = Dict(:Γ => Γ, :dΓ => dΓ, :n_Γ => n_Γ)
     merge!(params,force_params)
 
 end
+
+
+"""
+    print_on_request(log_dir::String)
+
+If in the directory `log_dir` exists and there is the `PrintSim.txt` file return true. If true is printing simulations results in Paraview format (.vtu and .pvtu). 
+"""
+function print_on_request(log_dir::String)
+    flag = false
+    current_dir = readdir()
+    log_idx = findfirst(x->x==log_dir, current_dir)
+    
+    if !isnothing(log_idx)
+        log_dir = readdir(current_dir[log_idx])
+        print_idx = findfirst(x->x =="PrintSim.txt", log_dir)
+        if !isnothing(print_idx)
+            flag = true
+        end
+    end
+    
+    return flag
+    end
---
+++ test/CommonsTests/ExportUtilitiesTests.jl
@@ -0,0 +1,41 @@
+# module ExportUtilitiesTests
+
+using SegregatedVMSSolver
+using PartitionedArrays
+using Gridap
+using GridapDistributed
+
+function export_utilities_test(rank_partition,distribute)
+    parts  = distribute(LinearIndices((prod(rank_partition),)))
+    domain = (0,1,0,1)
+    mesh_partition = (4,4)
+    model = CartesianDiscreteModel(parts,rank_partition,domain,mesh_partition)
+    order = 2
+    u((x,y)) = (x+y)^order
+    f(x) = -Δ(u,x)
+    reffe = ReferenceFE(lagrangian,Float64,order)
+    V = TestFESpace(model,reffe,dirichlet_tags="boundary")
+    U = TrialFESpace(u,V)
+    Ω = Triangulation(model)
+    dΩ = Measure(Ω,2*order)
+
+    Γ = BoundaryTriangulation(model; tags="boundary") 
+    n_Γ = get_normal_vector(Γ)
+    params = Dict(:Γ=>Γ,:n_Γ=>n_Γ, :parts=>parts, :force_tags=>["boundary"])
+
+    global_unique_idx = SegregatedVMSSolver.export_nodes_glob(parts, Γ)
+    local_unique_idx = SegregatedVMSSolver.get_local_unique_idx(parts, Γ)
+    SegregatedVMSSolver.export_n_Γ(params, local_unique_idx, global_unique_idx)
+end
+
+rank_partition = (2,2)
+with_debug() do distribute
+    export_utilities_test(rank_partition,distribute)
+end
+
+
+
+
+
+
+# end
