9c3f5cdbececaf09ae6b358bfd2ade7650c27a50
carlodev <carlobrunellibs@gmail.com>
2023-11-22 12:11:05 +0100

    rm blanks

--- test/CommonsTests/CommonsTests.jl
+++ test/CommonsTests/CommonsTests.jl
@@ -4,21 +4,12 @@ using Test
 
 @testset "Commons" begin
   include("InitializeParamsTests.jl")
-  
-#  include("CommonsProcedureTests.jl")
   include("AddNewTagsTests.jl")
-
   include("StabParamsTests.jl")
   include("LinearUtilitiesTests.jl")
   include("StabilizedEquationsTests.jl")
-  
-
   include("MatrixCreationTests.jl")
-  
   include("RestartTests.jl")
-#   include("ExportUtilitiesTests.jl")
-
-
 end
 
 
