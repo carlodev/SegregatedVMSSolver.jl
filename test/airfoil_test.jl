3c828b2763c96f11069b3229e4c8e5351b5ff9d8
carlodev <carlobrunellibs@gmail.com>
2023-12-01 09:05:22 +0100

    fix

--- test/airfoil_test.jl
+++ test/airfoil_test.jl
@@ -36,8 +36,8 @@ function run_airfoil_test(backend)
          :restart=> false,
          :restart_file=> restart_file,
          :printmodel=>false,
-         :name_tags=>["airfoil"]
-         :fieldexport=>[("friction","ph")]
+         :name_tags=>["airfoil"],
+         :fieldexport=>[("ph","friction")]
    )
    
    
