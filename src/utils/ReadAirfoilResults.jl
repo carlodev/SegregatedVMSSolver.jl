66d70dd9361fd74db2527f1a3022414a8d6c8ee9
carlodev <carlobrunellibs@gmail.com>
2024-04-08 11:34:31 +0200

    add more utilities for obtaining spectra

--- src/utils/ReadAirfoilResults.jl
+++ src/utils/ReadAirfoilResults.jl
@@ -4,11 +4,22 @@ using CSV
 using DataFrames
 using Trapz
 
+using Statistics
+using FFTW, LaTeXStrings
+using ScatteredInterpolation
+
+
+
 export get_nodes
 export get_normals
 export average_field
 export extract_airfoil_features
 export compute_CL_CD
+
+export compute_average
+export compute_average_2D
+export compute_plane_tke
+export compute_scatter_interp
 
 
 
@@ -244,6 +255,309 @@ function compute_CL_CD(top_nodesx,bottom_nodesx,top_nodesy,bottom_nodesy,cp_top,
     
     return CL,CD
 end
+
+
+
+
+
+
+
+###############################################
+## Read Fluctuations
+###############################################
+"""
+    ZProbe
+Give a 2D point coordinate xp, yp, it gived the idx of the points aligned in the z direction
+"""
+struct ZProbe
+    xp::Float64
+    yp::Float64
+    idx::Vector{Int64}
+end
+
+function ZProbe(df::DataFrame, xpyp::Vector{Float64})
+    xp,yp=xpyp
+    
+    df0 =filter(x->x.z ==0, df)
+    
+    dx = df0.x .- xp 
+    dy = df0.y .- yp
+    
+    _,idx0 = findmin(dx.^2 .+dy.^2)
+    xc0 = df0[idx0,:].x
+    yc0 = df0[idx0,:].y
+    
+
+    dfM=Matrix(df)
+
+    idx0x = findall(dfM[:,1].== xc0)
+    idx0y = findall(dfM[:,2].== yc0)
+    @assert idx0x==idx0y
+    
+   
+    z = dfM[idx0x,3]
+    zsort = sortperm(z)
+    ZProbe(xc0,yc0,idx0x[zsort])
+end
+
+"""
+    XYPlane
+Give a zp coordinate, it gives the indexes of the points in that XY plane
+"""
+struct XYPlane
+    zp::Float64
+    idx::Vector{Int64}
+end
+
+function XYPlane(df::DataFrame, zp::Float64)
+    zidx = findall(x-> x == zp, df.z)
+    tsort = zidx[sortperm(df[zidx,:])]
+    tsort_filter = tsort[df[tsort,:].x .>0.5]
+    XYPlane(zp,tsort_filter)
+end
+
+
+"""
+    get_idx_sort_reduct(res_path::String,tagname::String,offset::Int64,offend::Int64)
+
+In res_path directory, for the boundary tagname, it provides the sorted file indexes
+"""
+function get_idx_sort_reduct(res_path::String,tagname::String,offset::Int64,offend::Int64)
+    filenames = readdir(res_path)
+    idx_tag = findall(occursin.("$(tagname)_uh",filenames))
+
+    idx_sort = sortperm(filenames[idx_tag], by=custom_cmp_file)
+
+    if offend<0
+        offend = length(idx_sort)
+    else
+        @assert offend>offset
+        @assert offend<=length(idx_sort)
+    end
+    idx_sort_reduct = idx_tag[idx_sort[offset:offend]]
+
+    return filenames, idx_sort_reduct
+end
+
+
+"""
+    read_fluctuations(res_path::String, xpyp::Vector{Float64};tagname="topairfoil", offset=1,offend=-1)
+
+It is reading the results in `res_path`, for all the points aligned in Z direction of coordinates `xpyp`. 
+It provides the ZProbe which has the information of the actual nodes used.
+Vel_Mat is a dense matrix: Time x Points x Velocity Components (3)
+offset and offend can be used to skip intial and final files (avoiding OutOfMemory() error)
+"""
+function read_fluctuations(res_path::String, xpyp::Vector{Float64};tagname="topairfoil", offset=1,offend=-1)
+    
+    filenames, idx_sort_reduct = get_idx_sort_reduct(res_path,tagname,offset,offend)
+    tnodes = get_nodes(res_path; tagname=tagname)
+
+    zprobe = ZProbe(tnodes,xpyp)
+
+    println("Probe in point $(zprobe.xp), $(zprobe.yp), idxs=$(zprobe.idx) ")
+   
+    Vel_Mat = zeros(length(idx_sort_reduct),length(zprobe.idx),3) #time x point x velocity direction
+
+
+    for (i,fi) in enumerate(idx_sort_reduct)
+        fpath = joinpath(res_path,filenames[fi])
+        println(fpath)
+        df_tmp = CSV.read(fpath, DataFrame)[zprobe.idx,:]
+        Vel_Mat[i,:,:]=copy(Matrix(df_tmp))
+        
+    end
+    
+
+return zprobe,Vel_Mat
+end
+
+"""
+    compute_PSD(Vel::Array, tn::Vector{Float64})
+
+It computes the PSD of the TKE. Vel is the result obtained through `read_fluctuations`. The spectras are averaged in the Z direction 
+"""
+function compute_PSD(Vel::Array, tn::Vector{Float64})
+    VelMean = Statistics.mean(Vel,dims=1)
+
+    VelFluct = (Vel .- VelMean).^2
+
+    Ktime = 0.5 .* sum(VelFluct,dims=3)
+    Nz = size(Ktime)[2]
+
+    freqs_z = Vector[]
+    PSD_z = Vector[]
+
+    dt = tn[2] - tn[1]
+    nt=length(tn)
+
+
+    for i = 1:1:Nz
+        fhat=fft(Ktime[:,i,1])
+        PSD = fhat.*conj(fhat)/(nt)
+        PSD = real(fftshift(PSD))
+        freqs = fftshift(fftfreq(nt,1/dt))
+        push!(PSD_z,PSD)
+        push!(freqs_z,freqs)
+
+    end
+
+    return mean(PSD_z), freqs_z[1]
+end
+
+
+
+"""
+    compute_plane_tke(res_path::String;tagname="topairfoil", offset=1,offend=-1, zp=[0.1])
+
+It computes the TKE for each point in the plane with Z=Zp.
+"""
+function compute_plane_tke(res_path::String;tagname="topairfoil", offset=1,offend=-1, zp=[0.1])
+
+
+    filenames, idx_sort_reduct = get_idx_sort_reduct(res_path,tagname,offset,offend)
+    tnodes = get_nodes(res_path; tagname=tagname)
+    
+    Zidx = Vector[]
+    
+    nz = length(zp)
+    
+    for z in zp
+        zidx = XYPlane(tnodes, z).idx
+        push!(Zidx,zidx)
+    end
+    
+        
+    Vel_Mat = zeros(nz,length(idx_sort_reduct), length(Zidx[1]),3) #number of planes x times  x pointx x velocity direction
+    
+    for (i,fi) in enumerate(idx_sort_reduct)
+        fpath = joinpath(res_path,filenames[fi])
+        println(fpath)
+        for (j,zidx) in enumerate(Zidx)
+            df_tmp = CSV.read(fpath, DataFrame)[zidx,:]
+            Vel_Mat[j,i,:,:]=copy(Matrix(df_tmp))
+        end
+
+ 
+        
+    end
+    
+    Vel_uprime = Statistics.std(Vel_Mat,dims=2)
+    Vel_uprime2 = Vel_uprime .^2 
+    Vel_uprime2_zavg = Statistics.mean(Vel_uprime2,dims=1)
+    TKE = vec(0.5 .*sum(Vel_uprime2_zavg,dims=4))
+
+    return TKE, Vel_Mat
+    
+end
+
+##### Time Average
+
+function compute_average(res_path::String;tagname="topairfoil", offset=1,offend=-1)
+    
+    filenames, idx_sort_reduct = get_idx_sort_reduct(res_path,tagname,offset,offend)
+    Ntime = length(idx_sort_reduct)
+
+    fpath = joinpath(res_path,filenames[idx_sort_reduct[1]])
+    df_avg = CSV.read(fpath, DataFrame)./Ntime
+
+    for (i,fi) in enumerate(idx_sort_reduct[2:end])
+        fpath = joinpath(res_path,filenames[fi])
+        println(fpath)
+        df_tmp = CSV.read(fpath, DataFrame)
+        df_avg = df_avg .+ df_tmp./Ntime
+        
+    end
+    Vel_avg_3D=Matrix(df_avg)
+
+
+    return Vel_avg_3D
+end
+
+
+function compute_average_2D(Vel_avg3D::Matrix; tagname="topairfoil")
+    tnodes = get_nodes(res_path; tagname=tagname)
+
+    unique_z = unique(tnodes.z)
+    z_idx_sort = sortperm(unique_z)
+    unique_z[z_idx_sort]
+    
+    zpoints = findall(x-> x == unique_z[z_idx_sort[2]], tnodes.z)
+    Nzpoints = length(zpoints)
+    Nz = length(z_idx_sort) - 2
+    Vel_avg2D = zeros(Nzpoints,3)
+    
+
+    
+    for z in  unique_z[z_idx_sort[2:end-1]]
+        zidx = findall(x-> x == z, tnodes.z)
+        tsort = sortperm(tnodes[zidx,:])
+
+        M =     Vel_avg3D[zidx[tsort],:]
+        Vel_avg2D = Vel_avg2D .+ M ./ Nz
+    end
+
+    return Vel_avg2D
+end
+
+
+
+
+
+####Interpolate
+
+
+"""
+    compute_scatter_interp(res_path, velocity::Vector{Float64} ,zp::Float64; tagname="topairfoil", ylims=[-0.018,0.15], xlims=[0.5,1.0])
+
+In res_path is reading the nodes file associated with the tagname. It computes the interpolation of the values in velocity on nodes of plane in zp.
+NearestNeighbor() algorithm is used.
+"""
+function compute_scatter_interp(res_path, velocity::Vector{Float64} ,zp::Float64; tagname="topairfoil", ylims=[-0.018,0.15], xlims=[0.5,1.0])
+
+    tnodes = get_nodes(res_path; tagname=tagname)
+    
+
+    zidx = XYPlane(tnodes, zp).idx
+
+    tz0nodes = tnodes[zidx,1:2]
+    tz0nodesM=Matrix(tz0nodes)
+
+
+    
+    x = tz0nodes.x
+    y = tz0nodes.y
+    points = tz0nodesM'
+    
+    
+    
+    
+    # Create dense grid of points
+    x_dense = collect(range(xlims..., length=200))
+    
+    y_dense = vcat(collect((range(ylims..., length=250))))
+    
+    grid = collect(Iterators.product(x_dense, y_dense))
+    xgrid = vec(getindex.(grid,1))
+    ygrid = vec(getindex.(grid,2))
+    
+    
+    itp = ScatteredInterpolation.interpolate(NearestNeighbor(), points, velocity)
+    
+    velocity_dense = zeros(length(grid))
+    
+    for (i,p) in enumerate(grid)
+        pp = [p[1];p[2]]
+        interpolated = ScatteredInterpolation.evaluate(itp, pp)
+        velocity_dense[i] = interpolated[1]
+        println(i)
+    end
+    
+    return  xgrid,ygrid,velocity_dense
+    
+end
+
+
 
 
 end #end module
