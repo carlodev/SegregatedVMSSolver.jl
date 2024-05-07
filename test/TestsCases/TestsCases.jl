5c5d637f881e0c66f2a0ae69ed35c9bf85b8bb1c
carlodev <carlobrunellibs@gmail.com>
2024-05-07 12:00:58 +0200

    bug fix backend

--- test/TestsCases/TestsCases.jl
+++ test/TestsCases/TestsCases.jl
@@ -15,22 +15,22 @@ include("TaylorGreenTest.jl")
 function tests_cases(backend)
   backend() do distribute
     if backend == with_mpi
-        comm = MPI.COMM_WORLD
-        ranks = MPI.Comm_rank(comm)
-        #To avoid multiple printing of the same line in parallel
-        if ranks != 0
-          redirect_stderr(devnull)
-          redirect_stdout(devnull)
-        end
+      comm = MPI.COMM_WORLD
+      ranks = MPI.Comm_rank(comm)
+      #To avoid multiple printing of the same line in parallel
+      if ranks != 0
+        redirect_stderr(devnull)
+        redirect_stdout(devnull)
       end
     end
+  end
 
-    @testset "Cases Tests $(typeof(backend))" begin
-        airfoil_test(with_debug)
-        cylinder_test(with_debug)
-        liddriven_test(with_debug)
-        taylorgreen_test(with_debug)
-    end
+  @testset "Cases Tests $(typeof(backend))" begin
+    airfoil_test(backend)
+    cylinder_test(backend)
+    liddriven_test(backend)
+    taylorgreen_test(backend)
+  end
 
 end
 
