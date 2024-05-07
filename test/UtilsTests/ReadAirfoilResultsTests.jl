91a13e7ab9f3ff9066eec9b8278c89b78772caad
carlodev <carlobrunellibs@gmail.com>
2024-05-07 08:41:49 +0200

    comment Plot pkg

--- test/UtilsTests/ReadAirfoilResultsTests.jl
+++ test/UtilsTests/ReadAirfoilResultsTests.jl
@@ -3,7 +3,7 @@ module ReadAirfoilResultsTests
 using Test
 using SegregatedVMSSolver
 using SegregatedVMSSolver.ReadAirfoilResults
-using Plots
+# using Plots
 
 using DataFrames
 
@@ -30,13 +30,17 @@ Friction = time_space_average_field(res_path, "friction", nodes)
 @test typeof(Friction) <: DataFrame
 @test typeof(Velocity) <: DataFrame
 
-
 cp_top, cp_bottom = extract_Cp(nodes, Ph; u0=u0, rho=rho)
 friction_top, friction_bottom = extract_Cf(nodes, Friction, μ; u0=u0, rho=rho)
 
+@test typeof(cp_top) <: Vector{Float64}
+@test typeof(cp_bottom) <: Vector{Float64}
+@test typeof(friction_top) <: Vector{Float64}
+@test typeof(friction_bottom) <: Vector{Float64}
 
 CL,CD = compute_CL_CD(nodes, cp_top, cp_bottom,
 friction_top, friction_bottom; chord=1.0)
+
 
 @test typeof(CL)<:Real
 @test typeof(CD)<:Real
