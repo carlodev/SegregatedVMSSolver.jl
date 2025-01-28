761275b2201422b7c98d4dcad24d86f67db298d3
carlodev <carlobrunellibs@gmail.com>
2025-01-28 17:25:05 +0100

    testing parallel case codecov

--- test/UtilsTests/CreateVtuTests.jl
+++ test/UtilsTests/CreateVtuTests.jl
@@ -5,7 +5,7 @@ using DataFrames
 using SegregatedVMSSolver.CreateVtu
 
 
-fname = create_vtu_file("Results_vtu")
+fname = create_vtu_file(joinpath(@__DIR__,"Results_vtu"))
 @test typeof(fname) == String
 
 end #end module
--- test/sequential/runtests.jl
+++ test/sequential/runtests.jl
@@ -6,4 +6,3 @@ using Test
 
 @testset "Sequential" begin include( joinpath("SequentialTests.jl")) end
 
-@testset "Utils" begin  include(joinpath("..","UtilsTests","runtests.jl")) end
