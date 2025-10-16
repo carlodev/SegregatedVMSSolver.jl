d1a578ff8f487938a256a6d843a75608a692beb5
carlodev <carlobrunellibs@gmail.com>
2025-10-16 15:03:48 +0200

    restart from 3D solution

--- src/Commons/CreateProblem/Restart.jl
+++ src/Commons/CreateProblem/Restart.jl
@@ -6,11 +6,21 @@
 Create a BruteTree from the NearestNeighbors.jl package taking the coordinates points as input
 """
 function create_search_tree(restart_df::DataFrame)
+  data = []
+  s = 2 #it stores the info if the dataframe is 3D or 2D
+  if maximum(abs.(restart_df.uh_2)) > 1e-10
+    @info "3D Intialization - it can take a while"
+    data = vcat(restart_df.Points_0',restart_df.Points_1',restart_df.Points_2')
+    s = 3
+  else
+    @info "2D Intialization - third velocity component = 0"
+    data = vcat(restart_df.Points_0',restart_df.Points_1')
+    s = 2
+  end
 
-  data = vcat(restart_df.Points_0',restart_df.Points_1')
   brutetree = BruteTree(data; leafsize = 12)
 
-  return brutetree
+  return brutetree, s
 end
 
 
@@ -20,17 +30,24 @@ end
 
 It provides a suitable function which gives for each point the specified velocity in `restart_file`. It is used as initial condition for restarting a simulation at a specific time step.
 """
-function restart_uh_field(D::Int64,tree,restart_df::DataFrame)
+function restart_uh_field(D::Int64,s::Int64, tree,restart_df::DataFrame)
+  
+  if D == 2
+    s = 2
+  end
+
+  @assert s <= D "Restart DataFrame is $(s) dimensional, the dimension of the problem is $(D). Set uh_2 column == 0 to solve the problem"
 
   @info "Restarting uh0 ..."
-
   function u0(x)
-    p = [x...][1:2]
+    p = [x...][1:s]
     idx, _ = nn(tree, p)
     if D == 2
       return VectorValue(restart_df.uh_0[idx], restart_df.uh_1[idx])
-    elseif D==3
-      return VectorValue(restart_df.uh_0[idx],  restart_df.uh_1[idx], 0.0)
+    elseif D==3 && s == 2
+        return VectorValue(restart_df.uh_0[idx],  restart_df.uh_1[idx], 0.0)
+    elseif D == 3 && s == 3
+        return VectorValue(restart_df.uh_0[idx],  restart_df.uh_1[idx], restart_df.uh_2[idx])
     end
   end
 
